f88495eefa129e6680071cdbed244f61282498c4
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-27 10:58:31 +0100

    GO

--- first.go
+++ first.go
@@ -7,12 +7,9 @@ func main(){
 	fmt.Print("Enter a name: ")
 	var inputCollected string
 	fmt.Scanln(&inputCollected)
+	
 	var result string = "My name is "+ inputCollected
+	
 	fmt.Println(result)
-
-
-	
-
-	
 }
 
