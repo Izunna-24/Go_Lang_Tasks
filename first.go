34a9d6662fc9910a3256ca9a20257bf4604de304
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-27 10:55:46 +0100

    GO hard or GO hard

--- first.go
+++ first.go
@@ -1,6 +1,18 @@
 package main
 import "fmt"
-//
+
+
+
 func main(){
-	fmt.Println("Hello world")
+	fmt.Print("Enter a name: ")
+	var inputCollected string
+	fmt.Scanln(&inputCollected)
+	var result string = "My name is "+ inputCollected
+	fmt.Println(result)
+
+
+	
+
+	
 }
+
