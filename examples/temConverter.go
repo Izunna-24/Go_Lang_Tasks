ce22baa02a7a3f38716b58ebcacfbace5da08d65
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-27 12:27:43 +0100

    GO

--- examples/temConverter.go
+++ examples/temConverter.go
@@ -1,1 +1,13 @@
 package main	
+
+import "fmt"
+
+func main(){
+	fmt.Print("Enter degree in F: ")
+	var fareinheit int
+	fmt.Scanln(&fareinheit)
+
+	celcuis := (fareinheit - 32) * 5/9
+
+	fmt.Println("The equivalence of the temp you entered in celcuius is ", celcuis)
+}
