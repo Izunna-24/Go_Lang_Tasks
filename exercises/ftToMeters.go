9bb8478b546a925d645308ff75d0615b5aef4290
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-27 13:45:34 +0100

    GO

--- exercises/ftToMeters.go
+++ exercises/ftToMeters.go
@@ -2,11 +2,12 @@ package main
 import"fmt"
 
 func main(){
-	fmt.Println("Enter measurement in ft: ")
-	var ft float64
-	fmt.Scanf("%f", &ft)
+	// fmt.Println("Enter measurement in ft: ")
+	// var ft float64
+	// fmt.Scanf("%f", &ft)
 
-	var meters float64  = ft / 0.3048
+	// var meters float64  = ft / 0.3048
 
-	fmt.Println("measuremnt in meters: ",meters)
+	// fmt.Println("measuremnt in meters: ",meters)
 }
+
--- first.go
+++ first.go
@@ -3,13 +3,60 @@ import "fmt"
 
 
 
-func main(){
-	fmt.Print("Enter a name: ")
-	var inputCollected string
-	fmt.Scanln(&inputCollected)
+// func main(){
+// 	fmt.Print("Enter a name: ")
+// 	var inputCollected string
+// 	fmt.Scanln(&inputCollected)
 	
-	var result string = "My name is "+ inputCollected
+// 	var result string = "My name is "+ inputCollected
 	
-	fmt.Println(result)
-}
+// 	fmt.Println(result)
+// }
 
+// func main(){
+// 	fmt.Println(`
+// 	1
+// 	2
+// 	3
+// 	4
+// 	5
+// 	6
+// 	7
+// 	8
+// 	9
+// 	10`)
+// }
+
+// func main(){
+// 	i := 1
+// 	for i <=  10{
+// 		fmt.Println(i)
+// 		i += 1;
+// 	}
+// }
+
+// func main(){
+// 	for i := 1; i <= 10; i++{
+// 		if i % 2 == 0{
+// 		fmt.Println(i, "even")
+// 		}else{
+// 			fmt.Println(i, "odd")
+// 		}
+// 	}
+// }
+
+// func main(){
+// 	for i := 0; i <=10; i++{
+// 		if i == 0{
+// 			fmt.Println(i , "is zero")
+// 		}
+// 	}
+// }
+
+func main(){
+var i int = 0
+switch i {
+case 0: fmt.Println("zero")
+default :fmt.Println("cool")
+	}
+}
